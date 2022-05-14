be9485cfa86972cdbffb52f037dd1e3d776614cd
zubastu <79757684+zubastu@users.noreply.github.com>
2022-05-14 18:43:10 +0300

    добавил закрытие по эскейпу с корректной обработкой свнутри копонентов

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,21 +1,23 @@
-import React from "react";
+import React, {useEffect} from "react";
 
-const Popup = ({
-  closePopup,
-  children,
-  name,
-  selector,
-  isOpened
-}) => {
-
-
-
+const Popup = ({ closePopup, children, name, selector, isOpened }) => {
+  const closeOnEscapeKey = (e) => {
+    if (e.key === "Escape") {
+      closePopup(name);
+      document.removeEventListener("keyup", closeOnEscapeKey);
+    }
+  };
   const closeByOverlayClick = (e) => {
     if (e.target.classList.contains("popup_opened")) {
       closePopup(name);
     }
   };
-  const className = isOpened ? `${selector} popup_opened` : `${selector}`
+
+  const className = isOpened ? `${selector} popup_opened` : `${selector}`;
+
+  useEffect(() => {
+    isOpened && document.addEventListener("keyup", closeOnEscapeKey)
+  }, [isOpened])
 
   return (
     <div className={className} onClick={closeByOverlayClick}>
